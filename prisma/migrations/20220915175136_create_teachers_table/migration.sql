c25438a6668fd2c143ec1572b38eee27ac7efb15
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-15 14:51:44 -0300

    feat: create teachers table on database

---
+++ prisma/migrations/20220915175136_create_teachers_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "teachers" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "teachers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "teachers_name_key" ON "teachers"("name");
