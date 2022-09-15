2a2a40b062331e6f8a7648fa9d174e4afa5e5621
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-15 14:52:59 -0300

    feat: create disciplines table on database

---
+++ prisma/migrations/20220915175250_create_disciplines_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "disciplines" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "termId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "disciplines_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "disciplines_name_key" ON "disciplines"("name");
+
+-- AddForeignKey
+ALTER TABLE "disciplines" ADD CONSTRAINT "disciplines_termId_fkey" FOREIGN KEY ("termId") REFERENCES "terms"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
