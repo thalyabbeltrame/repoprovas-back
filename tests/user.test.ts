57b70661b170d69a92af9791ecf1a3648c097053
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-09-16 07:27:24 -0300

    feat: create tests for POST users/login route

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 import supertest from 'supertest';
+
 import { app } from '../src/app';
 import { userFactory } from './factories/userFactory';
 
 const client = new PrismaClient();
-
-console.log(process.env.NODE_ENV);
 
 beforeEach(async () => {
   await client.$executeRaw`TRUNCATE TABLE users;`;
@@ -42,6 +41,44 @@ describe('POST users/signup', () => {
     expect(response.body).toEqual({
       status: 'error',
       message: 'User already exists',
+    });
+  });
+});
+
+describe('POST users/login', () => {
+  it('Should return 200 and return a token', async () => {
+    const user = userFactory.createUserRequest();
+    await supertest(app).post('/users/signup').send(user);
+
+    const response = await supertest(app)
+      .post('/users/login')
+      .send({ email: user.email, password: user.password });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+  });
+
+  it('Should return 422 if body is invalid', async () => {
+    const user = userFactory.createUserRequest();
+    await supertest(app).post('/users/signup').send(user);
+
+    const response = await supertest(app)
+      .post('/users/login')
+      .send({ ...user, email: 'invalidemail' });
+
+    expect(response.status).toBe(422);
+  });
+
+  it('Should return 401 if user does not exist', async () => {
+    const user = userFactory.createUserRequest();
+    const response = await supertest(app)
+      .post('/users/login')
+      .send({ email: user.email, password: user.password });
+
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({
+      status: 'error',
+      message: 'Incorrect email/password',
     });
   });
 });
